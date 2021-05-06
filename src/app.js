517bb2526fb7da14da83a1cd01da49c5902940ef
unknown <almeadeen@gmail.com>
2021-05-06 09:45:29 +0300

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDirPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialesPath = path.join(__dirname, '../templates/partiales')
@@ -85,6 +87,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on pirt 3000.')
+app.listen(port, () => {
+    console.log('Server is up on pirt'+port)
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (lat, lon, callback) => {
-    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon='+ lon +'&appid=aa6a6458d300ccd968cdae56c7199065'
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon='+ lon +'&units=metric&appid=aa6a6458d300ccd968cdae56c7199065'
 
     request( {url, json: true}, (error,{body}) =>{
 
@@ -10,7 +10,7 @@ const forecast = (lat, lon, callback) => {
         } else if(body.message){
             callback('Unable to find location. Try another one.', undefined)
         }else{
-            callback(undefined, 'Tempreture is currently '+ body.main.temp)
+            callback(undefined, 'Tempreture is currently '+ body.main.temp +'C')
         }
     })
 }
